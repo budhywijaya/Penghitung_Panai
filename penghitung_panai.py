9123d188eca08812822e759213f211e32f1cdc54
budhywijaya <budiwijayarauf@gmail.com>
2024-06-08 08:27:00 +0800

    dokter and haji become input

--- penghitung_panai.py
+++ penghitung_panai.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     dokter_input = input("Apakah anda seorang dokter?(y/n) ")
     dokter_input = dokter_input.lower()
     if dokter_input == "iya" or dokter_input == "yes" or dokter_input == "y":
-    dokter = True
+        dokter = True
     elif dokter_input == "tidak" or dokter_input == "no" or dokter_input == "n":
         dokter = False
     else:
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     haji_input = input("Apakah anda sudah berhaji?(y/n) ")
     haji_input = haji_input.lower()
     if haji_input == "iya" or haji_input == "yes" or haji_input == "y":
-    haji = True
+        haji = True
     elif haji_input == "tidak" or haji_input == "no" or haji_input == "n":
         haji = False
     else:
